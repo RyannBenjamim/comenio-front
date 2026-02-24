fb31c4fe651b1eccc31ec3d414d15fdcd8343510
Ryan Costa <ryancosta0200119@gmail.com>
2026-02-24 18:13:12 -0300

    feat: modificao theme loader

--- public/scripts/theme-loader.js
+++ public/scripts/theme-loader.js
@@ -1,6 +1,7 @@
 (function() {
-  const savedTheme = localStorage.getItem("theme");
-  if (savedTheme === "dark") {
+  //const savedTheme = localStorage.getItem("theme");
+  const savedTheme = true
+  if (savedTheme === true) {
     document.documentElement.classList.add("dark");
   } else {
     document.documentElement.classList.remove("dark");
