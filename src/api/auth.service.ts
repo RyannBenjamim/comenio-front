aa4f299b442185052bd6afb7f7f1683a8c9c86ff
Ryan Costa <ryancosta0200119@gmail.com>
2026-02-26 16:11:56 -0300

    feat: modificao nas rotas

--- src/api/auth.service.ts
+++ src/api/auth.service.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export async function signin(email: string, senha: string) {
   const response = await axios.post(
-    //'https://comenio-api.vercel.app/api/auth/signin', 
-    'http://localhost:3000/api/auth/signin',
+    'https://comenio-api.vercel.app/api/auth/signin', 
+    //'http://localhost:3000/api/auth/signin',
     { email, senha }, 
     { headers: { 'Content-Type': 'application/json' } }
   );
